1090ad31cd1dd4f05660ea30d6a0f05895f21c30
vishaligunturi55 <vishaligunturi55@gmail.com>
2026-01-08 14:58:39 +0530

    variables.tf

---
+++ variables.tf
@@ -0,0 +1,10 @@
+variable "region" {
+  type    = string
+  default = "ap-south-1"
+}
+
+# Student should set this to their name/initials (example: "mouli")
+variable "student_id" {
+  type      = string
+  default   = "mouli"
+}
